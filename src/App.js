1020b8f1603b3c86a26b93b83442d5942080451d
Ben Wexler <mixedywex@gmail.com>
2021-09-10 21:21:08 -0400

    in progress

--- src/App.js
+++ src/App.js
@@ -6,56 +6,59 @@ import ReactDOM from 'react-dom'
 import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame, act, useThree } from '@react-three/fiber'
 import { Html, enter, MeshWobbleMaterial, OrbitControls } from '@react-three/drei';
-import { useSpring, useSpringRef, useChain } from '@react-spring/three'
+import { useSpring, useSprings, useSpringRef, useChain } from '@react-spring/three'
 import { a } from 'react-spring'
+import * as d3 from 'd3';
 
 const CardInfo = ({
   imgSrc,
   name,
   networth,
   opacity,
+  onClick,
+  // imgSrc,
 }) => {
   return (
-    <a.div className="wrapper">
-              <div className="box" style={{}}>
-              <div className="inner-box" style={{opacity}}>
-                <div className="top-row">
-                  <div className="img-container">
-                    <img
-                      className="img-prof-pic"
-                      src="https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D1699%26cropX2%3D3845%26cropY1%3D559%26cropY2%3D2704"
+    <a.div className="wrapper" onClick={onClick}>
+      <div className="box" style={{}}>
+        <a.div className="inner-box" style={{ opacity }}>
+          <div className="top-row">
+            <div className="img-container">
+              <img
+                className="img-prof-pic"
+                src={imgSrc}
 
-                    />
-                  </div>
+              />
+            </div>
 
-                  <div className="net-worth-amount">
-                    $151 Billion
+            <div className="net-worth-amount">
+              $151 Billion
                 </div>
-                </div>
+          </div>
 
-                <div
-                  style={{
-                    display: 'flex',
-                    flexGrow: 1,
-                  }}
-                >
-                  <div className="name-container"
-                  >
-                  <div>Elon Musk</div>
-                  {/* <div>Musk</div> */}
-                  </div>
-                  <button className="btn-view-profile">Read Full Profile</button>
+          <div
+            style={{
+              display: 'flex',
+              flexGrow: 1,
+            }}
+          >
+            <div className="name-container"
+            >
+              <div>{name}</div>
+              {/* <div>Musk</div> */}
+            </div>
+            <button className="btn-view-profile">Read Full Profile</button>
 
-                </div>
-              </div>
-              </div>
-            </a.div>
+          </div>
+        </a.div>
+      </div>
+    </a.div>
   )
-} 
+}
 
 
 
-const dimensions = [180, 90, 20.5]
+const dimensions = [140, 90, 20.5]
 const positions = {
   1: [0, 0, 0],
   2: [0, -60, -50],
@@ -65,9 +68,13 @@ const positions = {
   6: [0, -300, -250],
 }
 
+// const numCardsArr = Array.from(Array(10));
 
 
-const InnerApp = () => {
+
+
+
+const InnerApp = ({ count, setCount }) => {
 
 
   const ref = useRef()
@@ -93,94 +100,66 @@ const InnerApp = () => {
 
   // const activeSpringRef = useRef(1)
   const [activeSpring, setActiveSpring] = useState(1);
+  const offsetCoords = [0, -60, -50];
 
-  const endPositionPart1 = [0, -10, 60];
+  const endPositionPart1 = [0, -1100, -350];
+  
   // const durationSpring1 = 500;
-  const durationSpring1 = 500;
+  const durationSpring1 = 600;
 
   const spring1 = useSpring(
     {
+      // reset: true,
+      from: {
+        position: [0, -100, 0],
+        rotation: [degrees_to_radians(-140), 0, 0],
+      },
       ref: springRef1,
       onRest: () => {
-        setActiveSpring(2);
-        // activeSpringRef.current = 2;
-        console.log("1 ENDED")
+        setActiveSpring(2)
 
       },
-      to: {
-        // position: [0, -10, 80],
-        position: endPositionPart1,
-        rotation: [degrees_to_radians(-70), 0, 0],
-        opacity: 0,
-        // position: [0, -10, -40],
-        // rotation: [degrees_to_radians(-70), 0, 0],
+      position: endPositionPart1,
+      rotation: [degrees_to_radians(-140), 0, 0],
+      opacity: 0,
+      config: {
+        duration: durationSpring1,
       },
-      from: {
-        position: [0, 40, 20],
-        rotation: [degrees_to_radians(-120), 0, 0],
-        opacity: 1,
-        //      position: [0, -30, 80],
-        // rotation: [degrees_to_radians(-90), 0, 0],
-      },
-      config: { duration: durationSpring1 },
     })
 
 
 
   const spring2 = useSpring(
     {
+      // reset: true,
       delay: durationSpring1,
       ref: springRef2,
-      onRest: () => { console.log("2 ENDED") },
+      onRest: () => {
+        setActiveSpring(1)
+
+      },
       to: {
-        // position: [0, -10, 80],
-        // rotation: [degrees_to_radians(-90), 0, 0],
-        position: [0, 40, -60],
+        position: offsetCoords.map((coord, i) => coord + (offsetCoords[i] * 9)),
         rotation: [degrees_to_radians(-70), 0, 0],
         opacity: 1,
       },
       from: {
-        // position: [0, 30, 20],
-        // rotation: [degrees_to_radians(-70), 0, 0],
-        //  position: [0, -30, 80],
         position: endPositionPart1,
-        rotation: [degrees_to_radians(-90), 0, 0],
+        rotation: [degrees_to_radians(-150), 0, 0],
         opacity: 0,
       },
-      config: { duration: 500 },
+      config: {
+        duration: 800,
+        easing: d3.easeBackOut.overshoot(1.5),
+      },
     })
-  // const { position, rotation, opacity } = activeSpring === 1 ? spring1 : spring2
-  const { position, rotation, opacity } = spring1 
+  const { position, rotation, opacity } = activeSpring === 1 ? spring1 : spring2
+  // const { position, rotation, opacity } = spring1
 
 
-  const spring3 = useSpring(
-    {
-      // ref: springRef1,
-      onRest: () => {
-        // setActiveSpring(2);
-        // activeSpringRef.current = 2;
-        console.log("1 ENDED")
 
-      },
-      to: {
-        // position: [0, 60, 20],
-        position: [0, 40, -30],
-        // position: endPositionPart1,
-        rotation: [degrees_to_radians(-70), 0, 0],
-        // opacity: 0,
-        // position: [0, -10, -40],
-        // rotation: [degrees_to_radians(-70), 0, 0],
-      },
-      from: {
-        position: [0, 20, 0],
-        rotation: [degrees_to_radians(-70), 0, 0],
-        // opacity: 1,
-        //      position: [0, -30, 80],
-        // rotation: [degrees_to_radians(-90), 0, 0],
-      },
-      config: { duration: 400 },
-    })
-  const chain = useChain([springRef1, springRef2])
+  const chain = useChain([springRef1, springRef2], [0, 0])
+
   console.log('chain', chain)
 
 
@@ -213,6 +192,110 @@ const InnerApp = () => {
       config: { duration: 400 },
     })
 
+  const names = [
+    { name: 'Jeff Bezos', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D627%26cropX2%3D1639%26cropY1%3D129%26cropY2%3D1142', networth: 1, },
+    { name: 'Elon Musk', imgSrc: "https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D1699%26cropX2%3D3845%26cropY1%3D559%26cropY2%3D2704", networth: 1, },
+    { name: 'Bernard Arnault', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5dc05518ca425400079c659f%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D4000%26cropY1%3D1209%26cropY2%3D5212', networth: 1, },
+    { name: 'Bill Gates', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f4ebe0c87612dab4f12a597%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D292%26cropX2%3D3684%26cropY1%3D592%26cropY2%3D3987', networth: 1, },
+    { name: 'Mark Zuckerberg', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c76b7d331358e35dd2773a9%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D4401%26cropY1%3D0%26cropY2%3D4401', networth: 1, },
+    { name: 'Warren Buffet', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5babb7f1a7ea4342a948b79a%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D748%26cropX2%3D3075%26cropY1%3D1753%26cropY2%3D4082', networth: 1, },
+    { name: 'Larry Ellison', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5e8b62cfc095010007bffea0%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D4529%26cropY1%3D652%26cropY2%3D5184', networth: 1, },
+    { name: 'Larry Page', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c76bcaaa7ea43100043c836%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D227%26cropX2%3D2022%26cropY1%3D22%26cropY2%3D1817', networth: 1, },
+    { name: 'Sergey Brin', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c7d7c254bbe6f78090d831f%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D475%26cropX2%3D2887%26cropY1%3D168%26cropY2%3D2582', networth: 1, },
+    { name: 'Mukesh Ambani', imgSrc: 'https://thumbor.forbes.com/thumbor/190x190/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c7d7829a7ea434b351ba0b6%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D206%26cropX2%3D2043%26cropY1%3D250%26cropY2%3D2089', networth: 1, }
+
+  ]
+
+  const cards = names;
+
+  const originCoords = [0, 0, 0];
+  
+
+
+
+  //     const positions = originCoords.map((coord, j) => coord + (offsetCoords[j] * i);
+
+  //     var indexToSplit = arr.indexOf('c');
+  // var first = arr.slice(0, indexToSplit);
+  // var second = arr.slice(indexToSplit + 1);
+
+  const springs = useSprings(
+    cards.length,
+    cards.map((item, i) => {
+      let index = i - count
+
+      if (index < 0) {
+        index = cards.length + index;
+        // console.log('index', index)
+      }
+
+      const isLastCard = index === cards.length - 1;
+      return ({
+        from: {
+          opacity: 0,
+          position: count === 0 ? [0, 0, 0] : originCoords.map((coord, j) => coord + (offsetCoords[j] * i)),
+          rotation: [degrees_to_radians(-70), 0, 0],
+          index,
+        },
+        opacity: isLastCard ? 0 : index === 0 ? 1 : 0.2,
+        position: originCoords.map((coord, j) => {
+          // const offset = (j === 1 && index === cards.length - 1 ? 320 : 0);
+          const offset = 0;
+
+          return offset + coord + (offsetCoords[j] * index)
+        }),
+        // rotation: index === cards.length - 1 ? [degrees_to_radians(-160), 0, 0] : [degrees_to_radians(-70), 0, 0],
+        rotation: [degrees_to_radians(-70), 0, 0],
+        index,
+        config: {
+          duration: index === 9 ? 0 : isLastCard ? 1200 : 800,
+          easing: isLastCard ? d3.easeBackOut.overshoot(2) : d3.easeQuadInOut,
+        },
+
+      })
+    })
+  );
+
+  const createCards = (numCards, originCoords = [0, 0, 0], offsetCoords = [0, 0, 0]) => {
+
+    return (
+      springs.map((spring, i) => {
+        const { position, opacity, index } = spring;
+        console.log('spring', spring)
+        console.log('index', index)
+
+        if(index.animation.from === 9) return null;
+        // console.log('opacity', opacity)
+        return (
+          <Box
+            key={i}
+            rotation={spring.rotation} myRef={ref} position={position} dimensions={dimensions}>
+            <Html
+
+
+              className="content"
+              // position={[0, 0, 0]} 
+              position={[0, 0, 10.25]}
+              transform distanceFactor={400}>
+              <CardInfo
+                index
+                name={cards[i].name}
+                imgSrc={cards[i].imgSrc}
+                onClick={() => setCount(prevVal => {
+                  const newVal = prevVal + 1;
+                  return newVal >= cards.length ? 0 : newVal
+                })}
+
+                opacity={opacity} />
+            </Html>
+          </Box>
+
+        )
+      })
+    )
+
+
+  }
 
 
   return (
@@ -239,43 +322,32 @@ const InnerApp = () => {
       <group
         // position={[-60, 150, 50]}
         position={[0, 150, 50]}
-        >
-      
-        <Box rotation={rotation} myRef={ref} position={positions[1]} dimensions={dimensions}>
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={1} />
+      >
+
+        {createCards(10, [0, 0, 0], [0, -60, -50])}
+        <Box
+          // key={i}
+          rotation={rotation} position={position} dimensions={dimensions}>
+          <Html
+
+
+            className="content"
+            // position={[0, 0, 0]} 
+            position={[0, 0, 10.25]}
+            transform distanceFactor={400}>
+            <CardInfo
+              index
+              name={cards[count - 1]?.name}
+              imgSrc={cards[count - 1]?.imgSrc}
+              onClick={() => setCount(prevVal => {
+                const newVal = prevVal + 1;
+                return newVal >= cards.length ? 0 : newVal
+              })}
+
+              opacity={1} />
           </Html>
         </Box>
 
-        <Box rotation={rotation} position={positions[2]} dimensions={dimensions}>
-
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={0.2} />
-          </Html>
-        </Box>
-
-        <Box rotation={rotation} position={positions[3]} dimensions={dimensions}>
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={0.2} />
-          </Html>
-        </Box>
-        <Box rotation={rotation} position={positions[4]} dimensions={dimensions}>
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={0.2} />
-          </Html>
-        </Box>
-
-        <Box rotation={rotation} position={positions[5]} dimensions={dimensions}>
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={0.2} />
-          </Html>
-        </Box>
-        <Box rotation={rotation} position={positions[6]} dimensions={dimensions}>
-          <Html className="content" position={[0, 0, 0]} transform distanceFactor={400}>
-          <CardInfo opacity={0.2} />
-          </Html>
-        </Box>
-        
       </group>
       {/* </Center> */}
     </>
@@ -284,23 +356,58 @@ const InnerApp = () => {
 }
 
 function App() {
+  // const [cameraPosition, setCameraPosition] = useState([-60, 450, 180])
   const [cameraPosition, setCameraPosition] = useState([-60, 450, 180])
   const cameraRef = useRef();
 
-  // useEffect(() => {
-  //   cameraRef.current.position = cameraPosition
-  // }, [cameraPosition])
-  // const { camera } = useThree()
-  // useEffect(() => void camera.position.set(0, 0, 200), )
+  const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+
+  const innerAppProps = { count, setCount }
+
+  const [rankCoords, setRankCoords] = useState({ left: 0, top: 0 })
+
+  const handleResize = () => {
+    // return;
+    setTimeout(() => {
+      console.log('count', count)
+      const dimensions = document.querySelectorAll('.content')[countRef.current] ?.getBoundingClientRect();
+      // const dimensionsLast = document.querySelectorAll('.content')[9]?.getBoundingClientRect();
+      if (!dimensions) return;
+
+      setRankCoords({ left: dimensions.left, top: dimensions.top })
+
+      document.querySelector('.heading-rank').style.left = `${Math.round(dimensions.left)}px`
+      // document.querySelector('.heading-rank').style.top = `${Math.round(dimensions.top - 50)}px`
+    }, 0)
+
+    // console.log('dimensions', dimensions)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      handleResize()
+    }, 100)
+
+    window.addEventListener('resize', handleResize);
+  }, [])
+
+  useEffect(() => {
+    countRef.current = count;
+  }, [count])
+
+
   return (
     <div className="App" style={{ height: '100vh', display: 'flex', transform: 'scale(1.0)' }}>
-    <h1 className="heading-rank">#1</h1>
+      <h1
+        // style={{left: rankCoords.left, top: rankCoords.top}}
+        className="heading-rank">#{count + 1}</h1>
       <Canvas
         ref={cameraRef}
         // camera={{ position: [-5, 450, 240], fov: 45, }}
         camera={{ position: cameraPosition, fov: 45, }}
       >
-        <InnerApp />
+        <InnerApp {...innerAppProps} />
       </Canvas>
       <div style={{ position: 'absolute', bottom: 5, right: 5 }}>
         <div>X: <input type="number" value={cameraPosition[0]}
